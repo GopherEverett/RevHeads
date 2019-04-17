ac61f458c477348f24b98f4204dfd8d0e6b14ad3
John Jacobs <john.a.jacobs1973@gmail.com>
2019-04-17 09:59:05 -0400

    edit on project functional

---
+++ client/src/components/EditProjectForm.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+
+
+export default class EditProjectForm extends Component {
+    render() {
+        return (
+            <div>
+                <Form onSubmit={this.props.editProject}>
+                    <FormGroup>
+                        <Label for="title" hidden>Title</Label>
+                        <Input type="text" name="title" id="title" placeholder="Title" onChange={this.props.handleChange} value={this.props.project.title} />
+                    </FormGroup>
+                    {' '}
+                    <FormGroup>
+                        <Label for="date_begin" hidden>Date Started</Label>
+                        <Input type="text" name="date_begin" id="date_begin" placeholder="Date Started" onChange={this.props.handleChange} value={this.props.project.date_begin}/>
+                    </FormGroup>
+                    {' '}
+                    <FormGroup>
+                        <Label for="date_end" hidden>Date Finished</Label>
+                        <Input type="text" name="date_end" id="model" placeholder="Date Finished" onChange={this.props.handleChange} value={this.props.project.date_end}/>
+                    </FormGroup>
+                    {' '}
+                    <FormGroup>
+                        <Label for="details" hidden>Year</Label>
+                        <Input type="textarea" name="details" id="details" placeholder="Details" onChange={this.props.handleChange} value={this.props.project.details}/>
+                    </FormGroup>
+                    {' '}
+                    <FormGroup>
+                        <Label for="photo_url" hidden>Photo URL</Label>
+                        <Input type="text" name="photo_url" id="photo_url" placeholder="Photo URL" onChange={this.props.handleChange} value={this.props.project.photo_url}/>
+                    </FormGroup>
+                    {' '}
+                    <Button>Edit</Button>
+                </Form>
+            </div>
+        )
+    }
+}
--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -2,13 +2,16 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import { Button } from 'reactstrap'
 import { Redirect } from 'react-router-dom'
+import EditProjectForm from './EditProjectForm'
 
 export default class Project extends Component {
 
-state = {
-  project: {},
-  reDir: false
-}
+  state = {
+    project: {},
+    reDir: false,
+    isEditFormShow: false,
+    updatedProject: {}
+  }
 
   componentDidMount() {
     const projectId = this.props.match.params.id;
@@ -19,7 +22,8 @@ state = {
     try {
       const projectRes = await axios.get(`/api/v1/projects/${projectId}`)
       this.setState({
-        project: projectRes.data
+        project: projectRes.data,
+        isEditFormShow: false
       })
     }
     catch (err) {
@@ -29,28 +33,71 @@ state = {
 
   handleDelete = async () => {
     try {
-        await axios.delete(`/api/v1/projects/${this.props.match.params.id}`)
-        this.setState({
-            reDir: true
-        })
+      await axios.delete(`/api/v1/projects/${this.props.match.params.id}`)
+      this.setState({
+        reDir: true
+      })
     }
     catch (err) {
-        console.log(err)
+      console.log(err)
     }
+  }
+
+  handleChange = (evt) => {
+    const copyProject = { ...this.state.project }
+    copyProject[evt.target.name] = evt.target.value
+    this.setState({ project: copyProject })
+}
+
+  editProject = async (evt) => {
+    evt.preventDefault()
+    try {
+      await axios.put(`/api/v1/projects/${this.props.match.params.id}/`, {
+        title: this.state.project.title,
+        date_begin: this.state.project.date_begin,
+        date_end: this.state.project.date_end,
+        details: this.state.project.details,
+        photo_url: this.state.project.photo_url,
+        car: this.state.project.car
+      })
+      const projId = this.props.match.params.id
+      this.fetchProject(projId)
+
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+
+  toggleEditForm = () => {
+    this.setState((state) => {
+        return ({ isEditFormShow: !state.isEditFormShow })
+    })
 }
 
   render() {
     if (this.state.reDir === true) {
       return <Redirect to='/cars/' />
-  }
+    }
     return (
       <div>
-      <img src={this.state.project.photo_url} alt=''/>
+        {this.state.isEditFormShow ? 
+        <EditProjectForm  
+        project={this.state.project} 
+        handleChange={this.handleChange} 
+        editProject={this.editProject} 
+        fetchProject={this.fetchProject} />
+        : 
+        <div>
+        <img src={this.state.project.photo_url} alt='' />
         <p>{this.state.project.title}</p>
         <p>Date Started: {this.state.project.date_begin}</p>
         <p>Date Complete: {this.state.project.date_end}</p>
         <p>About... {this.state.project.details}</p>
         <Button color="danger" onClick={this.handleDelete}>{`Delete ${this.state.project.title}`}</Button>
+        <Button color='warning' onClick={this.toggleEditForm}>{`Edit ${this.state.project.title}`}</Button>
+        </div>
+        }
       </div>
     )
   }
